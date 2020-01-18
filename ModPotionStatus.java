dff5baa989b9e6ee7496a4c2c0244df7332c09e6
thund3rboi <56208524+thund3rboi@users.noreply.github.com>
2020-01-17 18:39:55 -0600

    Update ModPotionStatus.java

--- ModPotionStatus.java
+++ ModPotionStatus.java
@@ -1,9 +1,11 @@
-package yourpackage;
+package dev.micah.skyclient.mod.mods;
 
 import java.util.Collection;
 
 import org.lwjgl.opengl.GL11;
 
+import dev.micah.skyclient.hud.ScreenPosition;
+import dev.micah.skyclient.mod.ModDraggable;
 import net.minecraft.client.gui.FontRenderer;
 import net.minecraft.client.renderer.GlStateManager;
 import net.minecraft.client.renderer.RenderHelper;
@@ -61,9 +63,9 @@ public class ModPotionStatus extends ModDraggable {
                 {
                     s1 = s1 + " " + I18n.format("enchantment.level.4", new Object[0]);
                 }
-                font.drawString(s1, pos.getAbsoluteX(), pos.getAbsoluteY(), 16777215, true);
+                font.drawString(s1, pos.getAbsoluteX(), pos.getAbsoluteY() + i2, 16777215, true);
                 String s = Potion.getDurationString(potioneffect);
-                font.drawString(s, pos.getAbsoluteX(), pos.getAbsoluteY() + 10, 8355711, true);
+                font.drawString(s, pos.getAbsoluteX(), pos.getAbsoluteY() + i2 + 10, 8355711, true);
                 i2 += l;
             }
         }
@@ -72,49 +74,6 @@ public class ModPotionStatus extends ModDraggable {
 	public void renderDummy(ScreenPosition pos) {
 		font.drawString("Potion Effects", pos.getAbsoluteX(), pos.getAbsoluteY(), -1);
 	}
-	
-	/*
-	 * public void render(ScreenPosition pos) {
-		int i = 80;
-        int i2 = 16;
-        Collection<PotionEffect> collection = this.mc.thePlayer.getActivePotionEffects();
-
-        if (!collection.isEmpty())
-        {
-            GlStateManager.color(1.0F, 1.0F, 1.0F, 1.0F);
-            GlStateManager.disableLighting();
-            int l = 33;
-
-            if (collection.size() > 5)
-            {
-                l = 132 / (collection.size() - 1);
-            }
-
-            for (PotionEffect potioneffect : this.mc.thePlayer.getActivePotionEffects())
-            {
-                Potion potion = Potion.potionTypes[potioneffect.getPotionID()];
-                GlStateManager.color(1.0F, 1.0F, 1.0F, 1.0F);
-                String s1 = I18n.format(potion.getName(), new Object[0]);
-                if (potioneffect.getAmplifier() == 1)
-                {
-                    s1 = s1 + " " + I18n.format("enchantment.level.2", new Object[0]);
-                }
-                else if (potioneffect.getAmplifier() == 2)
-                {
-                    s1 = s1 + " " + I18n.format("enchantment.level.3", new Object[0]);
-                }
-                else if (potioneffect.getAmplifier() == 3)
-                {
-                    s1 = s1 + " " + I18n.format("enchantment.level.4", new Object[0]);
-                }
-                font.drawString(s1, (float)(i + 10 + 18 + 230), (float)(i2+ 6 + 20), 16777215, true);
-                String s = Potion.getDurationString(potioneffect);
-                font.drawString(s, (float)(i + 10 + 18 + 230), (float)(i2 + 6 + 30), 8355711, true);
-                i2 += l;
-            }
-        }
-	}
-	 */
 
 	@Override
 	public void save(ScreenPosition pos) {
